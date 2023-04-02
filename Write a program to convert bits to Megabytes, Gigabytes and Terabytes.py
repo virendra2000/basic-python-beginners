6f58473a25e9c59e7395d526f7c344a022c3a525
VK Community <vkalwar132000@gmail.com>
2023-04-02 11:43:26 +0530

    Add files via upload

---
+++ Write a program to convert bits to Megabytes, Gigabytes and Terabytes.py
@@ -0,0 +1,10 @@
+bit = int(input("Enter Data in bits\n"))
+#Megabytes
+Megabyte = bit * (1.25 * 10 ** -7)
+print("Megabtye of ",bit," is ",Megabyte)
+#Gigabytes
+Gigabyte = bit * (1.25 * 10 ** -10)
+print("Gigabyte of ",bit," is ",Gigabyte)
+#Terabytes
+Terabyte = bit * (1.25 * 10 ** -13)
+print("Terabyte of ",bit," is ",Terabyte)
